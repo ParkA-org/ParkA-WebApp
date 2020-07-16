371dcac526b6585d57a4947ded0dac76b785a3ff
Bant89 <bant8009@gmail.com>
2020-07-16 14:47:07 -0400

    prove vercel and github integration

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>ParkA WebApp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">the ParkA Next.js!</a>
         </h1>
 
         <p className="description">
