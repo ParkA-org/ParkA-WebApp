d430500126bd00ce611c925478e18257f38b2436
Bant89 <bant8009@gmail.com>
2020-09-14 15:29:39 -0400

    fix landing page scrolling bug

---
+++ pages/parking/checkout.tsx
@@ -0,0 +1,10 @@
+import Layout from "../layout"
+
+export default function Checkout() {
+
+    return (
+        <Layout pageTitle="Parking Checkout">
+            <h2>Checkout</h2>
+        </Layout>
+    )
+}
