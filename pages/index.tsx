103e629b5731adc05a8984f886c4a652a93fc29d
Tomas Familia <tfamilia@cardnet.com.do>
2020-10-06 16:55:38 -0400

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -323,7 +323,7 @@ function Download() {
 
 export default function Home(): JSX.Element {
   return (
-    <div>      
+    <div>
       <link
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossOrigin="anonymous" />
