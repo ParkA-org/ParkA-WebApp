c3cdb265580e2969ff11041c92ac6118fffe2776
Bant89 <bant8009@gmail.com>
2020-10-11 15:35:11 -0400

    really minor refactoring

--- components/VehicleSection/index.tsx
+++ components/VehicleSection/index.tsx
@@ -1,8 +1,5 @@
-import { UserContext } from "context/UserContext";
-import { useContext, useEffect, useState } from "react";
-import { useLazyQuery, useQuery } from "@apollo/client"
-import { GET_USER_ACCOUNT_DATA, GET_USER_VEHICLES, GET_ALL_VEHICLES } from "queries";
-import { Vehicle } from "utils/types"
+import { useQuery } from "@apollo/client"
+import { GET_ALL_VEHICLES } from "queries";
 import { BiPlusCircle } from "react-icons/bi";
 import NavigationLink from "components/NavigationLink"
 import VehicleCard from "components/VehicleCard"
@@ -13,32 +10,6 @@ import {
 } from "./styles"
 
 export default function VehicleSection() {
-    // const { userId } = useContext(UserContext)
-    // const [accountId, setAccountId] = useState("")
-    // const [getUserAccount, { data }] = useLazyQuery(GET_USER_ACCOUNT_DATA)
-    // const [getUserVehicles, { data: vehiclesData }] = useLazyQuery(GET_USER_VEHICLES)
-    // const [vehicles, setVehicles] = useState<Vehicle[]>([])
-    // useEffect(() => {
-    //     if (userId) {
-    //         getUserAccount({ variables: { id: userId } })
-    //     }
-    //     if (data) {
-    //         setAccountId(data.user.account_data.id)
-    //         console.log('Account id ', accountId)
-    //     }
-    // }, [data, userId])
-
-    // useEffect(() => {
-    //     if (accountId) {
-    //         getUserVehicles({ variables: { id: accountId } })
-    //     }
-
-    //     if (vehiclesData) {
-    //         console.log('DATA Vehiculos')
-    //         setVehicles(vehiclesData.accountDatum.vehicles)
-    //     }
-
-    // }, [vehiclesData, accountId])
     const { loading, error, data } = useQuery(GET_ALL_VEHICLES)
 
     if (error) return <h2>Error</h2>
